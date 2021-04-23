4c4f883fdd164315c9e9929b073367c1a3fdf600
badoi <badoi@me.com>
2021-04-23 02:48:57 -0400

    make final zoo annotations

---
+++ code/raw_code/make_polyfun_annotations/old_step3a_annotate_polyfun_zoonomia_phyloP_phast_HAR.sh
@@ -0,0 +1,109 @@
+SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
+GWASDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments
+ANNOTDIR=${GWASDIR}/polyfun/baselineLF2.2.UKB
+POLYFUNDIR='/home/bnphan/src/polyfun'
+DATADIR=${SETWD}/data/raw_data/zoonomia_annotations
+CODEDIR=${SETWD}/code/raw_code/make_polyfun_annotations
+ANNOTDIR=${DATADIR}/annotation
+cd $CODEDIR; mkdir -p $DATADIR $CODEDIR/logs $DATADIR/annotation
+
+conda activate polyfun
+
+#################################
+## 1) annotate the phyloP scores, phastcons, HARs
+LABEL=Zoonomia_phyloP_phast_HAR
+FILES=$(ls $DATADIR/bed/phyloPam*.hg19.bed.gz $DATADIR/bed/PhastCons*.hg19.bed.gz $DATADIR/bed/zoonomia_HARs_20210304.hg19.bed.gz | sed '/extend/d')
+for CHR in {1..22}; do
+## the final annotation file
+ANNOTFILE=${ANNOTDIR}/${LABEL}.${CHR}.annot.parquet
+if [[ ! -f $ANNOTFILE ]]; then
+for FILE in $FILES ; do
+echo "Extracting chr${CHR} for $(basename $FILE)."
+NAME=Zoonomia_$(basename $FILE .hg19.bed.gz | sed 's/zoonomia_HARs_20210304/HAR/g;s/_viterbi_conservedRegions//g')
+ANNOTFILE2=${ANNOTDIR}/241m.${NAME}.hg19.${CHR}.annot.parquet
+if [[ ! -f $ANNOTFILE2 ]]; then
+TMP=${DATADIR}/bed/${NAME}.${CHR}.tmp.bed.gz
+if [[ ! -f $TMP ]]; then zcat $FILE | awk -v CHR="chr${CHR}" '$1 == CHR {print}' | gzip > $TMP; fi
+python $POLYFUNDIR/make_annot_polyfun.py --annot-file ${ANNOTFILE2} --name ${NAME} \
+--bed-file ${TMP} --parquet-file ${DATADIR}/UKB_merged_snps.${CHR}.parquet
+rm $TMP
+fi; done
+## merge together the annotations together into 1 parquet file per chr
+PARQUET=$(ls ${ANNOTDIR}/241m.*.${CHR}.annot.parquet | tr '\n' ','| sed 's/,$//g')
+python $POLYFUNDIR/merge_annot_polyfun.py --parquet-in ${PARQUET} --annot-file ${ANNOTFILE}
+fi; done
+
+
+#####################################################################
+## 2) annotate the 500bp extensions phyloP scores, phastcons, HARs
+LABEL=Zoonomia_phyloP_phast_HAR_extend500
+FILES=$(ls $DATADIR/bed/phyloPam*.extend500.hg19.bed.gz $DATADIR/bed/PhastCons*.extend500.hg19.bed.gz $DATADIR/bed/zoonomia_HARs_20210304.extend500.hg19.bed.gz)
+for CHR in {1..22}; do
+## the final annotation file
+ANNOTFILE=${ANNOTDIR}/${LABEL}.${CHR}.annot.parquet
+if [[ ! -f $ANNOTFILE ]]; then
+for FILE in $FILES ; do
+echo "Extracting chr${CHR} for $(basename $FILE)."
+NAME=Zoonomia_$(basename $FILE .hg19.bed.gz | sed 's/zoonomia_HARs_20210304/HAR/g;s/_viterbi_conservedRegions//g')
+ANNOTFILE2=${ANNOTDIR}/ext500.${NAME}.hg19.${CHR}.annot.parquet
+if [[ ! -f $ANNOTFILE2 ]]; then
+TMP=${DATADIR}/bed/${NAME}.${CHR}.tmp.bed.gz
+if [[ ! -f $TMP ]]; then zcat $FILE | awk -v CHR="chr${CHR}" '$1 == CHR {print}' | gzip > $TMP; fi
+python $POLYFUNDIR/make_annot_polyfun.py --annot-file ${ANNOTFILE2} --name ${NAME} \
+--bed-file ${TMP} --parquet-file ${DATADIR}/UKB_merged_snps.${CHR}.parquet
+rm $TMP; fi; done
+## merge together the annotations together into 1 parquet file per chr
+ANNOTFILE1=${ANNOTDIR}/Zoonomia_phyloP_extend500.${CHR}.annot.parquet
+PARQUET1=$(ls ${ANNOTDIR}/ext500.*phyloPam*.${CHR}.annot.parquet | tr '\n' ','| sed 's/,$//g')
+if [[ ! -f $ANNOTFILE1 ]]; then python $POLYFUNDIR/merge_annot_polyfun.py --parquet-in ${PARQUET1} --annot-file ${ANNOTFILE1}; fi
+ANNOTFILE2=${ANNOTDIR}/Zoonomia_phast_HAR_extend500.${CHR}.annot.parquet
+PARQUET2=$(ls ${ANNOTDIR}/ext500.*PhastCons*.${CHR}.annot.parquet ${ANNOTDIR}/ext500.*HAR*.${CHR}.annot.parquet | tr '\n' ','| sed 's/,$//g')
+if [[ ! -f $ANNOTFILE2 ]]; then python $POLYFUNDIR/merge_annot_polyfun.py --parquet-in ${PARQUET2} --annot-file ${ANNOTFILE2}; fi
+## merge together the annotations together into 1 parquet file per chr
+if [[ -f $ANNOTFILE1 &&  -f $ANNOTFILE2 ]]; then python $POLYFUNDIR/merge_annot_polyfun.py --parquet-in ${ANNOTFILE1},${ANNOTFILE2} --annot-file ${ANNOTFILE}; fi
+fi; done
+
+
+############################################################
+# 3) merge the orig annotations together w/ the extended ###
+LABEL=Zoonomia_conservation
+for CHR in {1..22}; do
+ANNOTFILE=${ANNOTDIR}/${LABEL}.${CHR}.annot.parquet
+## merge together the annotations together into 1 parquet file per chr
+if [[ ! -f $ANNOTFILE ]]; then 
+PARQUET=${ANNOTDIR}/Zoonomia_phyloP_phast_HAR.${CHR}.annot.parquet,${ANNOTDIR}/Zoonomia_phyloP_phast_HAR_extend500.${CHR}.annot.parquet
+python $POLYFUNDIR/merge_annot_polyfun.py \
+--parquet-in ${PARQUET} --annot-file ${ANNOTFILE}; 
+fi; done
+
+
+#####################################################################
+## 4) add the base column calculate the M file from the annot parquet
+LABEL=Zoonomia_phyloP_phast_HAR
+for CHR in {1..22}; do 
+if [[ ! -f ${ANNOTDIR}/${LABEL}.${CHR}.l2.M ]]; then python ${POLYFUNDIR}/make_M_polyfun.py --parquet-file ${ANNOTDIR}/${LABEL}.${CHR}.annot.parquet --out ${ANNOTDIR}/${LABEL}.${CHR}; fi
+done
+
+LABEL=Zoonomia_conservation
+for CHR in {1..22}; do 
+if [[ ! -f ${ANNOTDIR}/${LABEL}.${CHR}.l2.M ]]; then python ${POLYFUNDIR}/make_M_polyfun.py --parquet-file ${ANNOTDIR}/${LABEL}.${CHR}.annot.parquet --out ${ANNOTDIR}/${LABEL}.${CHR}; fi
+done
+
+###############################################
+## 5) compute LD-scores for phyloP annotations
+LABEL=Zoonomia_phyloP_phast_HAR
+THECALL="source ~/.bashrc; conda activate polyfun; if [[ ! -f ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.l2.ldscore.parquet ]]; then python ${POLYFUNDIR}/compute_ldscores_ukb.py --annot ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.annot.parquet --ld-dir ${GWASDIR}/polyfun/LD_cache --out ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.l2.ldscore.parquet ; fi"
+sbatch --partition=pfen3 --time 23:00:00 --job-name=${LABEL} --mem=45G --array=1-22 --time 1-0:00:00 \
+--error=logs/run_annot_%A_%a.out.txt --output=logs/run_annot_%A_%a.out.txt --wrap="${THECALL}"
+
+LABEL=Zoonomia_conservation
+THECALL="source ~/.bashrc; conda activate polyfun; if [[ ! -f ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.l2.ldscore.parquet ]]; then python ${POLYFUNDIR}/compute_ldscores_ukb.py --annot ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.annot.parquet --ld-dir ${GWASDIR}/polyfun/LD_cache --out ${ANNOTDIR}/${LABEL}.\${SLURM_ARRAY_TASK_ID}.l2.ldscore.parquet ; fi"
+sbatch --partition=pfen3 --time 23:00:00 --job-name=${LABEL} --mem=45G --array=1-22 --time 1-0:00:00 \
+--error=logs/run_annot_%A_%a.out.txt --output=logs/run_annot_%A_%a.out.txt --wrap="${THECALL}"
+
+
+rm ${ANNOTDIR}/ext500.* ${ANNOTDIR}/241m.* ${ANNOTDIR}/*extend500* \
+${ANNOTDIR}/Zoonomia_phyloP_extend500*  ${ANNOTDIR}/Zoonomia_phast_HAR_extend500*
+
+
+
--- code/raw_code/make_polyfun_annotations/step1_liftover_phyloP_phastcons_regions.sh
+++ code/raw_code/make_polyfun_annotations/step1_liftover_phyloP_phastcons_regions.sh
@@ -1,54 +1,35 @@
 SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
 GWASDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments
-CACHEDIR=${GWASDIR}/polyfun/LD_cache
-ANNOTDIR=${GWASDIR}/polyfun/baselineLF2.2.UKB
-PHYLOPDIR=${GWASDIR}/phyloP/phyloP_cutoff_regions
+PHYLOPDIR=${GWASDIR}/phyloP
 DATADIR=${SETWD}/data/raw_data/zoonomia_annotations
 CODEDIR=${SETWD}/code/raw_code/make_polyfun_annotations
 CHAIN=/home/bnphan/resources/liftOver_chainz/hg38ToHg19.over.chain.gz
-CAUDATE_PEAKS=/projects/pfenninggroup/machineLearningForComputationalBiology/snATAC_cross_species_caudate/data/raw_data/hg38/Corces_2020/peak
-MONKEY_PEAKS=/projects/pfenninggroup/singleCell/Macaque_snATAC-seq/macaque_snATAC-seq/m015_Peanut_snATAC/data/raw_data/halper
-mkdir -p $DATADIR $DATADIR/bed $CODEDIR/logs; cd $CODEDIR
-
-#############################################################
-## get the phyloP scores, Corces2020 caudate scATAC peakset
-ALL_FILES=$(ls $CAUDATE_PEAKS/*narrowPeak.gz $PHYLOPDIR/*.bed.gz \
-	$MONKEY_PEAKS/*Macaca_mulattaToHomo_sapiens.HALPER.narrowPeak.gz | \
-	sed '/Consensus/d;/mappedTo/d;/UNK/d' )
+mkdir -p $DATADIR $DATADIR/bed_hg19 ${SETWD}/data/tidy_data/bed_hg38/HAR $CODEDIR/logs; cd $CODEDIR
 
 ######################
 ## map peaks to hg19
-for FILE in $ALL_FILES; do
-NAME=$(basename $FILE | sed 's/.narrowPeak.gz//g;s/.bed.gz//g;s/.GenBankRheMac8.Macaca_mulattaToHomo_sapiens.HALPER//g;s/Stauffer_striatum/Macaque_striatum/g;s/.hg38//g')
-if [[ ! -f $DATADIR/bed/${NAME}.hg19.bed.gz ]]; then 
-zcat $FILE > $DATADIR/bed/${NAME}.hg38.bed
-liftOver $DATADIR/bed/${NAME}.hg38.bed $CHAIN $DATADIR/bed/${NAME}.hg19.bed /dev/null
-gzip $DATADIR/bed/${NAME}.hg19.bed; rm $DATADIR/bed/${NAME}.hg38.bed
-fi
+HAR_FILES=$(ls $PHYLOPDIR/phyloP_cutoff_regions/*HARs_202104*.bed.gz )
+for FILE in $HAR_FILES; do
+NAME=$(basename $FILE | sed 's/.narrowPeak.gz//g;s/.bed.gz//g;s/.hg38//g')
+rsync $FILE ${SETWD}/data/tidy_data/bed_hg38/HAR
+FILE1500BP=${SETWD}/data/tidy_data/bed_hg38/HAR/$NAME.1500bp.bed.gz
+zcat $FILE | awk -F"\t" -v OFS="\t" -v PAD=750 '{MID = ($2 + $3)/2; $2 = MID - PAD; $3 = MID + PAD; print}' | gzip > $FILE1500BP
 done
 
-##############################################################
-## extend Zoonomia annotation peaks on either sides by 500bp
-HG19_CHR=/home/bnphan/resources/genomes/hg19/hg19.chrom.sizes
-FILES=$(ls $DATADIR/bed/*.hg19.bed.gz | sed '/extend/d;/Corces2020/d;/Macaque_striatum/d' )
-for FILE in ; do
-EXTFILE=$DATADIR/bed/$(basename $FILE .hg19.bed.gz).extend500.hg19.bed
-if [[ ! -f ${EXTFILE}.gz ]]; then 
-TMP=$DATADIR/bed/$(basename $FILE .gz).tmp
-TMP2=$DATADIR/bed/$(basename $FILE .gz).tmp2
-TMP3=$DATADIR/bed/$(basename $FILE .gz).tmp3
-zcat $FILE > $TMP
-bedtools slop  -i $TMP -g $HG19_CHR -b 500 > $TMP2
-sort -k1,1 -k2,2n $TMP2 > $TMP3
-bedtools merge -i $TMP3 > $EXTFILE
-gzip $EXTFILE; rm $TMP $TMP2 $TMP3
-fi; done
+###################################################################
+## get the phyloP regions, phastCons regions, C/HARs, ENCODE3 cCREs
+ALL_FILES=$(ls ${SETWD}/data/tidy_data/bed_hg38/HAR/*HARs_202104*.bed.gz \
+$PHYLOPDIR/phyloP_cutoff_regions/*241mam*.bed.gz \
+$PHYLOPDIR/phastCons_cutoff_regions/*43prim*.bed.gz \
+${SETWD}/data/tidy_data/bed_hg38/ENCODE3/*.bed.gz | sed '/viterbi/d' )
 
-
-######################################################
-### work on the zoonomia human accelerated regions ###
-## zoomHARs_named.bed received from Kathleen Keough March 3rd, 2021
-## liftover hg38 coordinates to hg19
-# cat $DATADIR/bed/zoomHARs_named.bed | sort -k 1,1 -k 2,2n | gzip > $DATADIR/bed/zoonomia_HARs_20210304.bed.gz
-# liftOver $DATADIR/bed/zoomHARs_named.bed $CHAIN $DATADIR/bed/zoonomia_HARs_20210304.hg19.bed /dev/null
-# gzip $DATADIR/bed/zoonomia_HARs_20210304.hg19.bed
+## map peaks to hg19
+for FILE in $ALL_FILES; do
+NAME=$(basename $FILE | sed 's/.narrowPeak.gz//g;s/.bed.gz//g;s/.hg38//g')
+echo "lifting ${NAME} to hg19"
+if [[ ! -f $DATADIR/bed_hg19/${NAME}.hg19.bed.gz ]]; then 
+zcat $FILE > $DATADIR/bed_hg19/${NAME}.hg38.bed
+liftOver $DATADIR/bed_hg19/${NAME}.hg38.bed $CHAIN $DATADIR/bed_hg19/${NAME}.hg19.bed /dev/null
+gzip $DATADIR/bed_hg19/${NAME}.hg19.bed; rm $DATADIR/bed_hg19/${NAME}.hg38.bed
+fi
+done
