161544d1c3218f13763ff528d34591ab6821ef03
badoi <badoi@me.com>
2021-04-26 21:04:22 -0400

    code to run final zoo annot + baseline

---
+++ code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF/step1a_calc_snpvars_polyfun_Zoonomia_annot_baselineLF.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --partition=pool3-bigmem,pfen3,pfen1
+#SBATCH --time 1-0:00:00
+#SBATCH --job-name=Asnpvars
+#SBATCH --mem=120G
+#SBATCH --error=logs/calc_snpvars_%A_%a.txt
+#SBATCH --output=logs/calc_snpvars_%A_%a.txt
+#SBATCH --array=1-8%3
+
+SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
+CACHEDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/LD_cache
+ANNOTDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/baselineLF2.2.UKB
+ZOONOMIADIR=${SETWD}/data/raw_data/zoonomia_annotations/annotation
+DATADIR=${SETWD}/data/raw_data/functional_polyfun_Zoonomia_annot_baselineLF
+CODEDIR=${SETWD}/code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF
+POLYFUNDIR='/home/bnphan/src/polyfun'
+
+cd $CODEDIR; 
+source ~/.bashrc; conda activate polyfun
+
+PREFIX=$(awk -F'\t' -v IND=${SLURM_ARRAY_TASK_ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
+N=$(awk -F'\t' -v IND=${SLURM_ARRAY_TASK_ID} 'FNR == IND + 1 {print $4}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)
+CUTOFF=5e-8
+
+echo "Working on ${PREFIX} GWAS with P < ${CUTOFF} cutoff for loci."
+
+##############################################################################################
+# https://github.com/omerwe/polyfun/wiki/1.-Computing-prior-causal-probabilities-with-PolyFun
+SUMSTATS=${SETWD}/data/tidy_data/polyfun/munged/${PREFIX}.parquet
+OUTDIR=${DATADIR}/${PREFIX}/snpvars
+mkdir -p $OUTDIR $DATADIR $CACHEDIR
+
+# 2. Run PolyFun with L2-regularized S-LDSC, using LF2.2.UKB
+if [ ! -f ${OUTDIR}/${PREFIX}.22.bins.parquet ]; then
+python ${POLYFUNDIR}/polyfun.py --compute-h2-L2 --allow-missing \
+--ref-ld-chr ${ZOONOMIADIR}/Zoonomia_annot_baselineLF. \
+--w-ld-chr ${ANNOTDIR}/weights.UKB. \
+--output-prefix ${OUTDIR}/$PREFIX --sumstats $SUMSTATS
+fi
+
+# sed -i 's/merged_baseline_zoonomia/Zoonomia_annot_baselineLF/g' $CODEDIR/step*
+# sed -i 's/Zoonomia_conservation_baselineLF/Zoonomia_annot_baselineLF/g' $CODEDIR/step*
---
+++ code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF/step3_runSusie_runAggregate_Zoonomia_annot_baselineLF.sh
@@ -0,0 +1,54 @@
+SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
+CACHEDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/LD_cache
+DATADIR=${SETWD}/data/raw_data/functional_polyfun_Zoonomia_annot_baselineLF
+CODEDIR=${SETWD}/code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF
+POLYFUNDIR='/home/bnphan/src/polyfun'
+CUTOFF=5e-8; cd $CODEDIR;
+
+
+#######################################
+## split finemapping job by chromosome 
+for ID in {1..24}; do
+PREFIX=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
+OUTDIR=${DATADIR}/${PREFIX}/susie
+# 23G good for blocks w/ < 15k SNPs, 47G good for blocks <30k snps
+if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF_aggregate.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+echo "Submitting job for ${PREFIX} GWAS.";
+sbatch -p pool1,pfen1 --mem 45G --time 24:00:00 --array 1-22 \
+--export=JobsFileName="${OUTDIR}/polyfun_all_jobs_@.txt" \
+${SETWD}/code/raw_code/nonfunct_finemapping/slurm_finemap_byLine.sh
+fi; done
+
+
+###########################################
+## combine all the jobs together per trait
+## catch the jobs that need a lot of RAM
+for ID in {1..24}; do
+PREFIX=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
+OUTDIR=${DATADIR}/${PREFIX}/susie
+## merge all jobs together
+JobsFileName=${OUTDIR}/polyfun_all_jobs.txt
+if [[ ! -f $JobsFileName  && -f ${OUTDIR}/polyfun_all_jobs_22.txt || $(wc -l $JobsFileName | cut -d ' ' -f1 ) == 0 ]]; then cat ${OUTDIR}/polyfun_all_jobs_*.txt > $JobsFileName; fi
+if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF_aggregate.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+echo "Submitting job for ${PREFIX} GWAS."; 
+sbatch -p pfen_bigmem,pool3-bigmem,pfen3 --mem 150G --time 1-00:00:00 --export=JobsFileName=${JobsFileName} \
+${SETWD}/code/raw_code/nonfunct_finemapping/slurm_finemap_byLine.sh
+fi; done
+
+
+##################################################
+## submit job to aggregate fine-mapping when done
+## catch the jobs that need a lot of RAM
+for ID in {1..24}; do
+PREFIX=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
+N=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $4}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)
+CUTOFF=5e-8
+SUMSTATS=${SETWD}/data/tidy_data/polyfun/munged/${PREFIX}.parquet
+OUTDIR=${DATADIR}/${PREFIX}/susie
+# if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF_top_annot.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+echo "Aggregating results from ${PREFIX} GWAS with P < ${CUTOFF} cutoff for loci."
+sbatch --export=OUTDIR=${OUTDIR},PREFIX=${PREFIX}_Zoonomia_annot_baselineLF,SUMSTATS=${SUMSTATS},CUTOFF=${CUTOFF} \
+--partition pfen1,pool1 --time 2:00:00 --mem 23G ${SETWD}/code/raw_code/nonfunct_finemapping/slurm_polyfun_aggregate.sh
+# fi; 
+done
+
