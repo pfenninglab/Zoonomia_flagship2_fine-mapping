32a17a89fe0b43fa48b34ab6094370a95e9db33e
badoi <badoi@me.com>
2021-05-13 07:24:50 -0400

    aggregate w missing jobs

--- code/raw_code/nonfunct_finemapping/slurm_polyfun_aggregate.sh
+++ code/raw_code/nonfunct_finemapping/slurm_polyfun_aggregate.sh
@@ -42,14 +42,16 @@ for CHR in {1..22}; do
 echo "Annotating SNPs on ${CHR} with PIP greater than ${PIP_CUTOFF}."
 FILE=${OUTDIR}/polyfun_all.chr${CHR}.gz
 OUT=$(echo $FILE | sed 's/polyfun_all/top_annot/g')
-if [[ ! -f $OUT || $OUT -ot $FINESNP ]]; then
+# if [[ ! -f $OUT || $OUT -ot $FINESNP ]]; then
 # find which column is CHR
 COL=$(zcat $PIPSNPS | awk -v col=CHR 'NR==1{for(i=1;i<=NF;i++) {if($i==col) {print i ;exit}} }')
 zcat $PIPSNPS | awk  -v COL=$COL -v VAR=${CHR} '{if( NR == 1 || $COL == VAR ) print }' | gzip > $FILE
 python ${POLYFUNDIR}/extract_annotations.py \
---annot ${ANNOTDIR}/Zoonomia_conservation_baselineLF.${CHR}.annot.parquet \
+--annot ${ANNOTDIR}/Zoonomia_annot_baselineLF.${CHR}.annot.parquet \
 --pips $FILE --pip-cutoff $PIP_CUTOFF --out $OUT
-rm $FILE; fi; done
+rm $FILE; 
+# fi;
+done
 cat ${OUTDIR}/top_annot.*.gz | zcat | awk '{if( NR == 1 || $2 ~ /^[0-9]+$/ ) print }' | gzip > $ANONTSNP
 fi; fi
 
