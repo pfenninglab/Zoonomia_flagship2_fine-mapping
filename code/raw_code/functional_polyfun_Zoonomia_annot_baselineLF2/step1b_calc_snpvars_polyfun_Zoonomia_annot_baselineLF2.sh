9d219ca9dedf5b4c715cef438de03bd77f11472f
badoi <badoi@me.com>
2021-05-13 07:27:30 -0400

    rerunning the merged annotations b/c polyfun merged wasnt right

---
+++ code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF2/step1b_calc_snpvars_polyfun_Zoonomia_annot_baselineLF2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --partition=pfen1,pool1
+#SBATCH --time 1-0:00:00
+#SBATCH --dependency=afterok:1606172
+#SBATCH --job-name=Bsnpvars
+#SBATCH --mem=45G
+#SBATCH --error=logs/calc_snpvars_%A_%a.txt
+#SBATCH --output=logs/calc_snpvars_%A_%a.txt
+#SBATCH --array=1-24%8
+
+SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
+CACHEDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/LD_cache
+ANNOTDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/baselineLF2.2.UKB
+ZOONOMIADIR=${SETWD}/data/raw_data/zoonomia_annotations/annotation
+DATADIR=${SETWD}/data/raw_data/functional_polyfun_Zoonomia_annot_baselineLF2
+CODEDIR=${SETWD}/code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF2
+POLYFUNDIR='/home/bnphan/src/polyfun'
+
+cd $CODEDIR; 
+source ~/.bashrc; conda activate polyfun
+
+PREFIX=$(awk -F'\t' -v IND=${SLURM_ARRAY_TASK_ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
+N=$(awk -F'\t' -v IND=${SLURM_ARRAY_TASK_ID} 'FNR == IND + 1 {print $4}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)
+CUTOFF=5e-8
+
+echo "Working on ${PREFIX} GWAS with P < ${CUTOFF} cutoff for loci."
+
+##############################################################################################
+# https://github.com/omerwe/polyfun/wiki/1.-Computing-prior-causal-probabilities-with-PolyFun
+SUMSTATS=${SETWD}/data/tidy_data/polyfun/munged/${PREFIX}.parquet
+OUTDIR=${DATADIR}/${PREFIX}/snpvars
+mkdir -p $OUTDIR $DATADIR $CACHEDIR
+
+# 3. Compute LD-scores for each SNP bin, do per chromosome
+for CHR in {1..22}; do
+if [[ ! -f ${OUTDIR}/${PREFIX}.${CHR}.l2.ldscore.parquet ]]; then
+python ${POLYFUNDIR}/polyfun.py --compute-ldscores --chr ${CHR} \
+--output-prefix ${OUTDIR}/$PREFIX --ld-ukb --ld-dir $CACHEDIR
+fi; done
+
+# 4. Re-estimate per-SNP heritabilities via S-LDSC
+if [[ ! -f ${OUTDIR}/${PREFIX}.22.snpvar_constrained.gz ]]; then
+python ${POLYFUNDIR}/polyfun.py --compute-h2-bins \
+--allow-missing --output-prefix ${OUTDIR}/$PREFIX \
+--sumstats $SUMSTATS --w-ld-chr ${ANNOTDIR}/weights.UKB.
+fi
+
--- code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF2/step3_runSusie_runAggregate_Zoonomia_annot_baselineLF2.sh
+++ code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF2/step3_runSusie_runAggregate_Zoonomia_annot_baselineLF2.sh
@@ -1,20 +1,20 @@
 SETWD='/projects/pfenninggroup/machineLearningForComputationalBiology/zoonomia_finemapping'
 CACHEDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnrichments/polyfun/LD_cache
-DATADIR=${SETWD}/data/raw_data/functional_polyfun_merged_baseline_zoonomia
-CODEDIR=${SETWD}/code/raw_code/functional_polyfun_merged_baseline_zoonomia
+DATADIR=${SETWD}/data/raw_data/functional_polyfun_Zoonomia_annot_baselineLF2
+CODEDIR=${SETWD}/code/raw_code/functional_polyfun_Zoonomia_annot_baselineLF2
 POLYFUNDIR='/home/bnphan/src/polyfun'
 CUTOFF=5e-8; cd $CODEDIR;
 
 
 #######################################
 ## split finemapping job by chromosome 
-for ID in {1..24}; do
+for ID in {1..8}; do
 PREFIX=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $2}' ${SETWD}/data/tidy_data/tables/readme_ukbb_gwas.tsv)"-Loh_2018"
 OUTDIR=${DATADIR}/${PREFIX}/susie
 # 23G good for blocks w/ < 15k SNPs, 47G good for blocks <30k snps
-if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_merged_baseline_zoonomia_aggregate.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF2_aggregate.txt.gz  ]]; then # && -f ${OUTDIR}/polyfun_all_jobs_22.txt
 echo "Submitting job for ${PREFIX} GWAS.";
-sbatch -p pool1,pfen1 --mem 45G --time 24:00:00 --array 1-22 \
+sbatch -p pool1,pfen1 --mem 45G --dependency=afterok:1606986 --time 24:00:00 --array 1-22 \
 --export=JobsFileName="${OUTDIR}/polyfun_all_jobs_@.txt" \
 ${SETWD}/code/raw_code/nonfunct_finemapping/slurm_finemap_byLine.sh
 fi; done
@@ -29,9 +29,9 @@ OUTDIR=${DATADIR}/${PREFIX}/susie
 ## merge all jobs together
 JobsFileName=${OUTDIR}/polyfun_all_jobs.txt
 if [[ ! -f $JobsFileName  && -f ${OUTDIR}/polyfun_all_jobs_22.txt || $(wc -l $JobsFileName | cut -d ' ' -f1 ) == 0 ]]; then cat ${OUTDIR}/polyfun_all_jobs_*.txt > $JobsFileName; fi
-if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_merged_baseline_zoonomia_aggregate.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF2_aggregate.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
 echo "Submitting job for ${PREFIX} GWAS."; 
-sbatch -p pfen_bigmem,pool3-bigmem,pfen3 --mem 150G --time 1-00:00:00 --export=JobsFileName=${JobsFileName} \
+sbatch -p pfen_bigmem,pool3-bigmem,pfen3 --mem 120G --time 1-00:00:00 --export=JobsFileName=${JobsFileName} \
 ${SETWD}/code/raw_code/nonfunct_finemapping/slurm_finemap_byLine.sh
 fi; done
 
@@ -45,10 +45,10 @@ N=$(awk -F'\t' -v IND=${ID} 'FNR == IND + 1 {print $4}' ${SETWD}/data/tidy_data/
 CUTOFF=5e-8
 SUMSTATS=${SETWD}/data/tidy_data/polyfun/munged/${PREFIX}.parquet
 OUTDIR=${DATADIR}/${PREFIX}/susie
-if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_merged_baseline_zoonomia_top_annot.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
+if [[ ! -f ${DATADIR}/${PREFIX}/${PREFIX}_Zoonomia_annot_baselineLF2_top_annot.txt.gz && -f ${OUTDIR}/polyfun_all_jobs_22.txt ]]; then
 echo "Aggregating results from ${PREFIX} GWAS with P < ${CUTOFF} cutoff for loci."
-sbatch --export=OUTDIR=${OUTDIR},PREFIX=${PREFIX}_merged_baseline_zoonomia,SUMSTATS=${SUMSTATS},CUTOFF=${CUTOFF} \
---partition pfen1,pool1 --time 2:00:00 --mem 23G ${SETWD}/code/raw_code/nonfunct_finemapping/slurm_polyfun_aggregate.sh
+sbatch --export=OUTDIR=${OUTDIR},PREFIX=${PREFIX}_Zoonomia_annot_baselineLF2,SUMSTATS=${SUMSTATS},CUTOFF=${CUTOFF} \
+--partition interactive,short1 --time 2:00:00 --mem 23G ${SETWD}/code/raw_code/nonfunct_finemapping/slurm_polyfun_aggregate.sh
 fi; 
 done
 
