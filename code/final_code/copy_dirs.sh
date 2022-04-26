1858c272859b1d38b906512ead2e220954164dbc
badoi <badoi@me.com>
2022-04-26 13:07:51 -0400

    download and prepare new gwas to fine-mapp

--- code/final_code/copy_dirs.sh
+++ code/final_code/copy_dirs.sh
@@ -3,6 +3,12 @@ GWASDIR=/projects/pfenninggroup/machineLearningForComputationalBiology/gwasEnric
 
 cd $SETDIR
 
-rsync -Pav $GWASDIR/polyfun/munged $SETDIR/data/tidy_data/polyfun
-mkdir -p $SETDIR/data/tidy_data/tables
-cp $GWASDIR/gwas/unmunged_sumstats/UKBB_BOLTLMM/zreadme_ukbb_gwas.tsv $SETDIR/data/tidy_data/tables/
+# rsync -Pav $GWASDIR/polyfun/munged $SETDIR/data/tidy_data/polyfun
+# mkdir -p $SETDIR/data/tidy_data/tables
+# cp $GWASDIR/gwas/unmunged_sumstats/UKBB_BOLTLMM/zreadme_ukbb_gwas.tsv $SETDIR/data/tidy_data/tables/
+
+
+## upload to BU_Addiction_snRNA-seq/Striatum
+https://drive.google.com/drive/folders/1aep9c9bIPAf3itNsz2apOnA6T0QbPFv6?usp=sharing
+DATADIR=$SETDIR/data/raw_data/zoonomia_annotations/annotation
+gdrive upload --recursive --parent 1aep9c9bIPAf3itNsz2apOnA6T0QbPFv6 $DATADIR/to_upload
